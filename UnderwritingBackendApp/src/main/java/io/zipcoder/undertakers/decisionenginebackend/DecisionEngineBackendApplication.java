b418dca4078b38a41a0f8e5cb3939aa50d61888b
Gregory Furlong <gnfurlong@gmail.com>
2015-10-15 15:30:52 -0400

    Initialize Underwriting App Backend

---
+++ UnderwritingBackendApp/src/main/java/io/zipcoder/undertakers/decisionenginebackend/DecisionEngineBackendApplication.java
@@ -0,0 +1,12 @@
+package io.zipcoder.undertakers.decisionenginebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionEngineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionEngineBackendApplication.class, args);
+    }
+}
---
+++ UnderwritingBackendApp/src/test/java/io/zipcoder/undertakers/decisionenginebackend/DecisionEngineBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package io.zipcoder.undertakers.decisionenginebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DecisionEngineBackendApplication.class)
+@WebAppConfiguration
+public class DecisionEngineBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
