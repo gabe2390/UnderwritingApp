0f81efa0ba4a39118f90c37d408edb186aa02d5e
alexbrown <abrown@192.168.1.18>
2015-10-16 14:45:31 -0400

    r

--- UnderwritingClientApp/app/features/response/js/ResponseCtrl.js
+++ UnderwritingClientApp/app/features/response/js/ResponseCtrl.js
@@ -1,7 +1,7 @@
 angular.module('underwritingClientAppApp')
   .controller('ResponseCtrl', function ($scope, Request) {
   $scope.requests = Request.getResponses().data;
-  $scope.$watch(function(Request){return Request.getResponse;}, function(newVal, oldVal) {
+  $scope.$watch(function(){return Request.getResponses();}, function(newVal, oldVal) {
 		if (typeof newVal !== 'undefined') {
 	$scope.requests = newVal;
         console.log(newVal);
